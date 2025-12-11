8d1a3bdc983520beb19eb0d1b39bb52bfb84cf64
sascott9655 <samscott027@gmail.com>
2025-12-11 09:19:57 -0800

    restarted my approach with nfl database loading. At least with team names and list not using endpoints web scraping.

--- espn_nfl_scraper.py
+++ espn_nfl_scraper.py
@@ -1,13 +1,4 @@
-# credit to https://github.com/pseudo-r/Public-ESPN-API 
-# section 5 for providing espn API
-
-#using nfl env with python version 3.12
-'''
-Docstring for espn_nfl_scraper
-Populates Teams, Seasons, Weeks, Games, TeamStats using 
-ESPN hidden API endpoints
-'''
-
+import json
 import time  #https://www.geeksforgeeks.org/python/python-time-module/
 import requests #https://www.w3schools.com/python/module_requests.asp
 import mysql.connector #https://www.w3schools.com/python/python_mysql_getstarted.asp
@@ -16,67 +7,34 @@ from tqdm import tqdm #progress bar for scraping data(it takes a while)
 
 #I have created a local sql database nfl_db so I will link to the database here
 
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "!Woody1013!", #need to properly encrypt password
-    "database": "nfl_db",
-}
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="*********",
+    database="nfl_db"
+)
 
-#shows espn that I am not a robot scraping data. Using data for hobby purposes.
-HEADERS = {
-    "User-Agent": "nfl-scraper/1.0 (+https://yourdomain.example) Python/requests"
-}
+with open("teams.json") as f:
+    teams = json.load(f)
 
-#season we are scraping
-SEASONS = [2020, 2021, 2022, 2023, 2024, 2025]
-
-#pause between requests to avoid hammering ESPN
-REQUEST_SLEEP = 0.3
-
-#espn endpoints from github
-SCOREBOARD = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
-TEAMS_LIST = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams"
-BOXSCORE = "https://https://cdn.espn.com/core/nfl/boxscore"
-
-# ========= DB HELPER FUNCTIONS ==========
-
-def db_connect():
-    return mysql.connector.connect(**DB_CONFIG)
 
 def execute(conn, sql, params=None):
     cur = conn.cursor()
     cur.execute(sql, params or ())
-    r = cur.fetchone()
+    conn.commit()  
     cur.close()
-    return r
-
-def fetchall(conn, sql, params=None):
-    cur = conn.cursor()
-    cur.execute(sql, params or ())
-    r = cur.fetchall()
-    cur.close()
-    return r
-
-# =========== UTIL ==========
-def safe_get(url, params=None, retries=3, backoff=1.0):
-    attempt = 0
-    while attempt < retries:
-        try:
-            r = requests.get(url, params=params, headers=HEADERS, timeout=15)
-            if r.status_code == 200: # means success status code
-                return r.json()
-            else:
-                print(f"HTTP {r.status_code} for {r.url}")
-        except Exception as e:
-            print("Request error:", e)
-        attempt += 1
-        time.sleep(backoff * attempt)
-    return None
-
-#understanding of safe_get -> it will extract the data needed from espn
-#it will attempt to do so 3 times, if it is safe to do so it will return
-#the data in json format. Otherwise it will tell you the status code error
-#and it will increase the wait time of retrieving the data by backoff*attempts 
 
 
+for team in teams:
+    execute(conn, """
+        INSERT INTO Teams (team_id, abbrev, name, city, logo)
+        VALUES (%s,%s,%s,%s,%s)
+        ON DUPLICATE KEY UPDATE
+            abbrev=VALUES(abbrev),
+            name=VALUES(name),
+            city=VALUES(city),
+            logo=VALUES(logo)
+    """, (team["team_id"], team["abbrev"], team["name"], team["city"], team["logo"]))
+
+conn.commit()  # save changes
+conn.close()
